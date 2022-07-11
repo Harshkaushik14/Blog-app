8282a63f9c77f2f0a39a654d2b7b63db4f36fb09
Harshkaushik14 <kaushikharsh86@gmail.com>
2022-07-11 10:30:11 +0530

    search bar added

--- components/NavBar.js
+++ components/NavBar.js
@@ -25,8 +25,23 @@ const NavBar = () => {
 </div>
     </div>
     <div className=" hidden lg:flex items-center cursor-not-allowed space-x-5 text-blue-500">
-      <h3>Sign In</h3>
-      <h3 className='border px-4 py-1 rounded-full border-black'>Get Started</h3>
+ 
+
+<div className='search'>
+<form>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-gray-300">Search</label>
+    <div class="relative">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input type="search" id="default-search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Articles..." required/>
+
+    </div>
+</form>
+</div>
+
+     
+      <h3 className='border px-4 py-1 rounded-full border-black'>Sign In</h3>
     </div>
 <div className="lg:hidden items-center">
 <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
